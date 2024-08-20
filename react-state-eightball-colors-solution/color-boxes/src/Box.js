b9b7143f26c4ac673748b2586230f7df5f813e4e
ivoriTal <s21italiferro@gmail.com>
2024-08-20 12:59:02 -0400

    first commit

---
+++ react-state-eightball-colors-solution/color-boxes/src/Box.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Box.css";
+
+
+function Box({ color }) {
+  return (
+      <div
+          className="Box"
+          style={{ backgroundColor: color }}
+      />
+  );
+}
+
+export default Box;
